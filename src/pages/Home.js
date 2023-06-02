f2915b55b11ca6c824449e4340dc0679551351bd
Lucas Capart <lucas.capart80@gmail.com>
2023-06-02 16:45:13 +0200

    Update Home.js
    Ajout de la partie 'Card' qui permet d'afficher les différents logements sur la page d'accueil (home)

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,23 +2,34 @@ import React from 'react';
 import Navigation from '../components/Navigation';
 import Logo from '../components/Banner';
 import Footer from '../components/Footer';
+import TopBackground from '../components/TopBackground';
+import Card from '../components/Card';
+import data from '../Logement.json';
 
 const Home = () => {
+    const nombreLogementAAfficher = 6;
+    const logementsAAfficher = data.slice(0, nombreLogementAAfficher);
+
     return (
-        <body>
+        <div className='stylePrincipal'>
             <header>
                 <Logo />
                 <Navigation />
             </header>
 
             <main>
-
+                <TopBackground />
+                <div className="app">
+                    {logementsAAfficher.map((logement, index) => (
+                        <Card key={logement.id} nom={logement.title} photo={logement.cover} className={`card-${index + 1}`} />
+                    ))}
+                </div>
             </main>
 
             <footer>
                 <Footer />
             </footer>
-        </body>
+        </div>
     );
 };
 
