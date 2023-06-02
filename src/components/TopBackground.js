e0560b525f8313af8bb835ed25f0f92b43cb50e7
Lucas Capart <lucas.capart80@gmail.com>
2023-06-02 16:46:22 +0200

    Create TopBackground.js
    création du component TopBackground.js

---
+++ src/components/TopBackground.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TopBackground = () => {
+    return (
+        <div className='topBackground'>
+            <img src="./imageSource1.png" alt="" />
+            <h1>Chez vous, partout et ailleurs</h1>
+        </div>
+    );
+};
+
+export default TopBackground;
