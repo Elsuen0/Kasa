c7933b50734e8fd370ccd4f3e83a7b9a13491167
Lucas Capart <lucas.capart80@gmail.com>
2023-06-05 16:06:24 +0200

    Create Logements.js
    Création de la page logements

---
+++ src/pages/Logements.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import FicheLogement from '../components/FicheLogement';
+
+const Logements = () => {
+    return (
+        <div>
+            <FicheLogement />
+        </div>
+    );
+};
+
+export default Logements;
