80667c50430bd69828e6b5c9ec775b8c247af221
Lucas Capart <lucas.capart80@gmail.com>
2023-06-06 17:34:19 +0200

    Update Banner.js
    Modification chemin d'accès image

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Navigation from '../components/Navigation';
 
 const Logo = () => {
     return (
         <div className="logo">
-            <img src="./logokasa.png" alt="Logo Kasa" />
+            <img src="/logokasa.png" alt="Logo Kasa" />
+            <Navigation />
         </div>
     );
 };
