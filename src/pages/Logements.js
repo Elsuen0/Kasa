6cfcef4213a68cb5ecd0c7d86f6b93c2b47254dd
Lucas Capart <lucas.capart80@gmail.com>
2023-06-07 16:11:43 +0200

    Update Logements.js
    Mise à jour du composant Logement -> Ajout d'une condition pour afficher ou non la page error 404

--- src/pages/Logements.js
+++ src/pages/Logements.js
@@ -2,21 +2,39 @@ import React from 'react';
 import Logo from '../components/Banner';
 import Footer from '../components/Footer';
 import FicheLogement from '../components/FicheLogement';
+import data from '../Logement.json';
+import { useParams } from 'react-router-dom';
+import Error404 from './Error404';
 
 const Logements = () => {
+    const { id } = useParams();
+    const logement = data.find(logement => logement.id === id);
+
     return (
         <div>
-            <header>
-                <Logo />
-            </header>
+            {
+                logement ? (
 
-            <main>
-                <FicheLogement />
-            </main>
+                    <div>
+                        <header>
+                            <Logo />
+                        </header>
 
-            <footer>
-                <Footer />
-            </footer>
+                        <main>
+                            <FicheLogement />
+                        </main>
+
+                        <footer>
+                            <Footer />
+                        </footer>
+                    </div >
+
+                ) : (
+                    <div>
+                        {/* Message d'erreur ou redirection */}
+                        <Error404 />
+                    </div>
+                )}
         </div>
     );
 };
