f07fe56e2c2d0b7cedf3f2eaac74837dd1716fe1
Lucas Capart <lucas.capart80@gmail.com>
2023-06-08 15:30:40 +0200

    Create Collapse.js
    Création du composant collapse

---
+++ src/components/Collapse.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronUp } from '@fortawesome/free-solid-svg-icons';
+
+const Collapse = ({ content, name }) => {
+    const [isCollapsed, setIsCollapsed] = useState(true);
+
+    const toggleCollapse = () => {
+        setIsCollapsed(!isCollapsed);
+    };
+
+    return (
+        <div className='collapse'>
+            <div className='collapse_title'>
+                <p>{name}</p>
+                <button onClick={toggleCollapse} className={isCollapsed ? 'collapsed' : 'expanded'}>
+                    <FontAwesomeIcon icon={faChevronUp} />
+                </button>
+            </div>
+            <div className={`collapse_content ${isCollapsed ? 'collapsed' : 'expanded'}`}>
+                {!isCollapsed && (
+                    <p>{content}</p>
+                )}
+            </div>
+        </div>
+    );
+};
+
+export default Collapse;
