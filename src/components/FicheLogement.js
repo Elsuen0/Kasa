688a07afc5ecd98de8b6ac0c429f7cac3ebfd368
Lucas Capart <lucas.capart80@gmail.com>
2023-06-12 16:00:32 +0200

    Update FicheLogement.js
    Création des 3 lignes qui comprennent les infos de chaque logement

--- src/components/FicheLogement.js
+++ src/components/FicheLogement.js
@@ -2,6 +2,11 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import data from '../Logement.json';
 import Carrousel from './Carrousel';
+import Profiluser from './Profiluser';
+import TitleLogement from './TitleLogement';
+import Tag from './Tag';
+import Rating from './Rating';
+import Collapse from './Collapse';
 
 const FicheLogement = () => {
     const { id } = useParams();
@@ -19,6 +24,22 @@ const FicheLogement = () => {
                     <h1>Logement non trouvé</h1>
                 </div>
             )}
+            <div className='firstLine'>
+                <TitleLogement title={logement.title} subTitle={logement.location} />
+                <Profiluser id={logement.id} profileImg={logement.host.picture} hostName={logement.host.name} />
+            </div>
+
+            <div className="secondLine">
+                <Tag />
+                <Rating rating={logement.rating} />
+            </div>
+
+            <div className="thirdLine">
+                <Collapse className='collapse_logement collapse' name="Description" content={logement.description} />
+                <Collapse className='collapse_logement collapse' name="Équipements" content={logement.equipments.map((equipment, index) => (
+                    <ul key={index}>{equipment}</ul>
+                ))} />
+            </div>
         </div>
     );
 };
