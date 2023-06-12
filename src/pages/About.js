16484908068d7bc6e56149d03f9894fcc8b8c024
Lucas Capart <lucas.capart80@gmail.com>
2023-06-12 15:58:34 +0200

    Update About.js
    Ajout de className personnalisés pour le composant collapse

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,7 +6,7 @@ import Collapse from '../components/Collapse';
 
 const About = () => {
     return (
-        <div>
+        <div className='stylePrincipal'>
             <header>
                 <Logo />
             </header>
@@ -14,10 +14,10 @@ const About = () => {
             <main className='main_about'>
                 <TopBackground className='topBackground topBackground_about' image={'./kalen.png'} />
                 <div>
-                    <Collapse name='Fiabilité' content='Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes.' />
-                    <Collapse name='Respect' content='La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.' />
-                    <Collapse name='Service' content="Kasa est site de réservation de logement qui facilite la recherche et la réservation de logements en ligne, offrant une plateforme sécurisée pour les voyageurs et les propriétaires. Vos informations sont cryptées et protégées par des mesures de sécurité avancées, garantissant la confidentialité de vos données personnelles et de vos transactions. Notre équipe travaille continuellement pour maintenir la fiabilité et l'intégrité de notre plateforme, afin de vous offrir une expérience de réservation sereine et sans souci" />
-                    <Collapse name='Sécurité' content="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standarts sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes." />
+                    <Collapse className={'collapse'} name='Fiabilité' content='Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes.' />
+                    <Collapse className={'collapse'} name='Respect' content='La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.' />
+                    <Collapse className={'collapse'} name='Service' content="Kasa est site de réservation de logement qui facilite la recherche et la réservation de logements en ligne, offrant une plateforme sécurisée pour les voyageurs et les propriétaires. Vos informations sont cryptées et protégées par des mesures de sécurité avancées, garantissant la confidentialité de vos données personnelles et de vos transactions. Notre équipe travaille continuellement pour maintenir la fiabilité et l'intégrité de notre plateforme, afin de vous offrir une expérience de réservation sereine et sans souci" />
+                    <Collapse className={'collapse'} name='Sécurité' content="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standarts sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes." />
                 </div>
             </main>
 
